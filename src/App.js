bf1c0a174ff99252d6bd256b93db523acd320239
Khalid Ahmed <khalidxah@Khalids-MacBook-Air.local>
2021-03-30 23:43:40 +0200

    Header Completed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Components/Navbar";
 import Header from "./Components/Header";
+import Particles from "react-particles-js";
 
 function App() {
   return (
     <>
+      <Particles 
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "f9ab00"
+              }
+            }
+          }
+        }}
+
+      />
       <Navbar />
       <Header />
     </>
